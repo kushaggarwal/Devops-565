83e42bc1116b7482a966ce3d963e01c370842cca
kushaggarwal <kush.aggarwal110085@gmail.com>
2023-10-31 06:59:13 +0530

    Terraform modules

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,24 +1,32 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
+#   owners = ["099720109477"] # Canonical
+# }
 
-resource "aws_instance" "web1" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.small"
+# resource "aws_instance" "web1" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t3.small"
 
-  tags = {
-    Name = "HelloWorld54"
-  }
+#   tags = {
+#     Name = "HelloWorld54"
+#   }
+# }
+
+module "ec2-instance" {
+  source = "./modules/ec2-instance"
+  ami = "ami-01eccbf80522b562b"
+  instance_type = "t3.micro"
+  instance_name = "myvm1"
+  
 }
